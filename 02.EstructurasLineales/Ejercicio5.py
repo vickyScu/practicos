2c66615e45309baa8f13ebe5c94d2e006f7577b9
vickyScu <mariascuderi@uca.edu.ar>
2026-09-16 02:00:52 -0300

    Add files via upload
    Cambie para que se pueda hacer con archivos json

--- 02.EstructurasLineales/Ejercicio5.py
+++ 02.EstructurasLineales/Ejercicio5.py
@@ -1,4 +1,6 @@
-def T_Sort(Q,gradoEntrada,E):
+import json
+
+def T_Sort(Q,gradoEntrada,P,E):
     ST = [] 
     while (len(Q) != 0 ): 
         T = Q.pop(0) #saco el elemento en la primera posicion pq trabajo con colas (FIFO)
@@ -8,17 +10,18 @@ def T_Sort(Q,gradoEntrada,E):
             if gradoEntrada[vecino] == 0: 
                 Q.append(vecino)
 
-    if len(ST) == len(E): 
+    if len(ST) == len(P): 
         return ST 
     return "Hay ciclo"
 
-def funcion_auxiliar(Q,gradoEntrada,E): 
-    for nodo in E: #recorro el grafo y en el diccionario guardo cuantos grados de entrada tiene 
+def funcion_auxiliar(Q,gradoEntrada,P,E): 
+    for nodo in P: #recorro el grafo y en el diccionario guardo cuantos grados de entrada tiene 
             gradoEntrada[nodo] = 0
     
     for nodo in E: 
         for vecino in E[nodo]: 
             gradoEntrada[vecino] +=1
+
     #meto todos los minimales a Q
     for nodo in gradoEntrada: 
         if gradoEntrada[nodo] == 0: 
@@ -27,26 +30,18 @@ def funcion_auxiliar(Q,gradoEntrada,E):
 
 
 def main(): 
-    E = {
-        "Algebra":     {"AnalisisI"}, # apunta a AnalisisI
-        "AnalisisI":   {"AnalisisII", "Fisica"},
-        "AnalisisII":  set(),
-        "Fisica":      {"Termo"},
-        "Termo":       set(),
-    }
+    with open("archivo.json") as f:
+        datos = json.load(f)
+
+    P = datos["P"]   # lista de nodos
+    E = datos["E"]   # diccionario nodo (lista de vecinos)
 
     Q=[]
     gradoEntrada = {}
 
-    funcion_auxiliar(Q,gradoEntrada,E)
-    print("T-SORT: ",T_Sort(Q,gradoEntrada,E))
+    funcion_auxiliar(Q,gradoEntrada,P,E)
+    print("T-SORT: ",T_Sort(Q,gradoEntrada,P,E))
 
-    E2 = {"A": {"B"}, "B": {"C"}, "C": {"A"}}
-    Q2 = [] 
-    gradoEntrada2 = {}
-    funcion_auxiliar(Q2,gradoEntrada2,E2)
-    print("T-SORT: ",T_Sort(Q2,gradoEntrada2,E2))
-    
 main()
 
     
